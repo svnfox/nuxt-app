35b6fd7075bc1566232662d00e398639789dc72b
卢胜抗 <lushengkang@th99.com>
2024-04-25 10:55:22 +0800

    feat: 测试页面

---
+++ pages/login.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="login">
+        <input type="text">请输入账号</input>
+        <button>点击登录</button>
+    </div>
+</template>
+
+<script lang="ts" setup>
+definePageMeta({
+    layout: 'empty'
+})
+</script>
+
+<style></style>
