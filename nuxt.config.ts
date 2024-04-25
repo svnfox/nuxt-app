a9ca73f69f9c6cacfc45c61f9b45ba78d3786a6e
卢胜抗 <lushengkang@th99.com>
2024-04-25 11:04:58 +0800

    chore: 配置html中的head

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      link: [{ rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css' }]
+    }
+  },
   devtools: { enabled: true },
   $production: {
     routeRules: {
